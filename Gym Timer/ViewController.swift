2671636c7f45e2136db2406a2ae85b81020dfdea
Pradnya <pradnyanikam@gmail.com>
2019-11-21 11:44:40 +0800

    Adds default values and rearranges the UI

--- Gym Timer/ViewController.swift
+++ Gym Timer/ViewController.swift
@@ -8,55 +8,86 @@
 
 import UIKit
 
+
+let DEFAULT_WORK_INTERVAL = 45
+let DEFAULT_REST_INTERVAL = 15
+
 class ViewController: UIViewController {
 
   var timer = Timer()
 
+  //User defined values
+  var workIntervalInSeconds = DEFAULT_WORK_INTERVAL
+  var restIntervalInSeconds = DEFAULT_REST_INTERVAL
+  
+  //Counters
+  var workIntervalCounter = DEFAULT_WORK_INTERVAL
+  var restIntervalCounter = DEFAULT_REST_INTERVAL
+
+  //Flags
+  
   var isWorkInterval = true
   var isStarted = true
   
-  var workIntervalInSeconds = 45
-  var restIntervalInSeconds = 15
-  @IBOutlet weak var timerLabel: UILabel!
-  @IBOutlet weak var statusLabel: UILabel!
+  //Outlets
+  @IBOutlet weak private var timerLabel: UILabel!
+  @IBOutlet weak private var statusLabel: UILabel!
+  @IBOutlet weak private var workIntervalLabel: UILabel!
+  @IBOutlet weak private var restIntervalLabel: UILabel!
   
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    resetValues()
   }
 
-  @IBAction func startTimerAction(_ sender: Any) {
-    runTimer()
+  private func resetValues() {
+    workIntervalLabel.text = "\(workIntervalInSeconds) seconds"
+    restIntervalLabel.text = "\(restIntervalInSeconds) seconds"
+    isWorkInterval = true
+    
   }
   
-  func runTimer() {
+  private func runTimer() {
     statusLabel.text = isWorkInterval ? "Working" : "Resting"
     timer = Timer.scheduledTimer(timeInterval: 1 , target: self,   selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
   }
   
-  @objc func updateTimer() {
+  @objc private func updateTimer() {
     let seconds : Int
     if isWorkInterval {
-      workIntervalInSeconds -= 1
-      seconds = workIntervalInSeconds
+      workIntervalCounter -= 1
+      seconds = workIntervalCounter
     } else {
-      restIntervalInSeconds -= 1
-      seconds = restIntervalInSeconds
+      restIntervalCounter -= 1
+      seconds = restIntervalCounter
     }
     if seconds < 2 {
       toggleTimer()
     }
     timerLabel.text = "\(seconds)"
-    
   }
+
   func toggleTimer() {
     timer.invalidate()
     isWorkInterval = !isWorkInterval
-    workIntervalInSeconds = 45 + 1
-    restIntervalInSeconds = 15 + 1
+    workIntervalCounter = workIntervalInSeconds + 1
+    restIntervalCounter = restIntervalInSeconds + 1
     runTimer()
   }
   
   
+  //MARK: Button actions
+  @IBAction func startTimerAction(_ sender: Any) {
+    runTimer()
+  }
+  @IBAction func stopTimerAction(_ sender: Any) {
+    timer.invalidate()
+    resetValues()
+  }
+  
+  @IBAction func changeIntervalsAction(_ sender: Any) {
+    
+  }
 }
 
